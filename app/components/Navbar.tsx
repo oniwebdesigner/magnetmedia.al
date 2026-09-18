65984991a9c1023f5bd434a93b9748aa7339dc18
Parjon Terpollari <parjonterpollari5@gmail.com>
2026-09-18 19:55:24 +0100

    logo test

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -50,7 +50,7 @@ export default function Navbar() {
        <Link href="/" className="flex items-center gap-2.5">
   {/* eslint-disable-next-line @next/next/no-img-element */}
  <Image
-  src="/logo.png"
+  src="/logo/logo.png"
   alt="Magnet Media"
   width={64}
   height={64}
